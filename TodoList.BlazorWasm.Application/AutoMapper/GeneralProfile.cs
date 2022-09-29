53915cfb762c92f0dd634079c238653e00da2dd7
Huỳnh Quang Vinh <vinhhq@esuhai.com>
2022-09-29 10:15:57 +0700

    add automapper

---
+++ TodoList.BlazorWasm.Application/AutoMapper/GeneralProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TodoList.BlazorWasm.Application.Features.Todos.Commands.CreateTodos;
+using TodoList.BlazorWasm.Application.Features.Todos.Queries.GetTodosByUserId;
+using TodoList.BlazorWasm.Domain.Entities;
+
+namespace TodoList.BlazorWasm.Application.AutoMapper
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<CreateTodosCommand, TodosList>();
+            CreateMap<TodosList, CreateTodosViewModel>();
+            CreateMap<TodosList, GetTodosByUserIdQueryViewModel>();
+            CreateMap<TodosList, GetTodosQueryViewModel>();
+        }
+    }
+}
